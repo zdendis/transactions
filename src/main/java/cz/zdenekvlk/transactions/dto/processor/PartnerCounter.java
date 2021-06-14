5fac084ea1da4a9cd4b1a29785408d0e049a2c8f
zdeny <vlk.zdenek@gmail.com>
2021-06-14 17:02:15 +0200

    Partner transaction number init

--- src/main/java/cz/zdenekvlk/transactions/dto/processor/PartnerCounter.java
+++ src/main/java/cz/zdenekvlk/transactions/dto/processor/PartnerCounter.java
@@ -3,11 +3,18 @@ package cz.zdenekvlk.transactions.dto.processor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class PartnerCounter {
-    private final Map<String, Integer> partnerCounts = new ConcurrentHashMap<>();
+    private final Map<String, Integer> partnerCounts;
+    private final Map<Integer, Integer> partnerOrder;
+
+    public PartnerCounter() {
+        partnerCounts = new ConcurrentHashMap<>();
+        partnerOrder = new TreeMap<>();
+    }
 
     public void add(String partner) {
         Integer count = partnerCounts.get(partner);
@@ -17,4 +24,6 @@ public class PartnerCounter {
     public Integer get(String partner) {
         return partnerCounts.get(partner);
     }
+
+    //TODO create comparable by date list
 }
